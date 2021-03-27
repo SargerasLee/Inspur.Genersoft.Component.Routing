16b92da68ae332dd1e4ada0c2833c7ed07170896
captain <1328008364@qq.com>
2021-03-27 13:27:47 +0800

    测试用例

---
+++ Inspur.Genersoft.Component.UnitTest/Component/MyComponent1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inspur.Genersoft.Component.Routing;
+using Inspur.Genersoft.Component.Routing.Attributes;
+using Inspur.Genersoft.Component.Routing.Core;
+
+namespace Inspur.Genersoft.Component.UnitTest.Component
+{
+	[CustomComponent]
+	[RouteMapping("/test/comp1")]
+	public class MyComponent1
+	{
+		[RouteMapping("/{code}/{name}")]
+		[Json]
+		public Dictionary<string, string> Test1([RouteParam]string code, [RouteParam]string name)
+		{
+			return new Dictionary<string, string> { { "code", code }, { "name", name } };
+		}
+
+		[RouteMapping("/change")]
+		public object Test2([UrlParam]int age)
+		{
+			return age;
+		}
+
+		[RouteMapping("/write")]
+		public object Test3(RouteContext context)
+		{
+			return context.UrlParams;
+		}
+	}
+}
